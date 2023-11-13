14a6a781f0742d6026d28445780cc62ad36d7d7f
dquangv <dquangvu7998@gmail.com>
2023-11-13 23:54:24 +0700

    Upload lab 6 (6.1) - JDBC

---
+++ labs/Lab6.sql
@@ -0,0 +1,26 @@
+﻿drop database Java3_Lab6_PS36680;
+go
+
+create database Java3_Lab6_PS36680;
+go
+
+use Java3_Lab6_PS36680;
+go
+
+create table books (
+id int identity not null,
+title nvarchar(50),
+price float,
+primary key (id));
+go
+
+insert into books values
+(N'Lập trình C', 100),
+(N'Lập trình Java', 200),
+(N'Lập trình C#', 150);
+go
+
+select *
+from books
+where title like N'%Lập trình%';
+go
---
+++ labs/database/Lab6.sql
@@ -0,0 +1,6 @@
+create database Java3_Lab6_PS36680;
+go
+
+use Java3_Lab6_PS36680;
+go
+
